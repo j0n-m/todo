2f5e6fc9e0fde7b85863e6cf8fe5070e1a288432
Jon Monarrez <135302142+j0n-m@users.noreply.github.com>
2023-08-12 22:31:10 -0500

    Add styling

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import dates from 'date-fns';
 import './style.css';
+import domController from './domController';
 
 function Task(propObj) {
   let title = propObj.title;
@@ -44,34 +45,106 @@ function Project(propObj) {
   function getTitle() {
     return title;
   }
-  return { tasks, title, setTitle, getTitle };
+  function addTask(taskObj) {
+    tasks.push(taskObj);
+  }
+  return { tasks, title, setTitle, getTitle, addTask };
 }
+const pubSub = (function () {
+  const events = {};
+  function subscribe(eventName, func) {
+    if (!events[eventName]) {
+      events[eventName] = [];
+    }
+    events[eventName].push(func);
+  }
+  function publish(eventName, paramData) {
+    if (events[eventName]) {
+      events[eventName].forEach(e => {
+        e(paramData);
+      });
+    }
+  }
+  return { subscribe, publish }
+})();
 
 const todoController = (function () {
   const projects = [];
 
-  initialize();
   function initialize() {
-    // if (localStorage.getItem('projects')) {
-    //   projects.push(JSON.parse(localStorage.getItem('projects')));
-    // }
-    if (!projects.length) {
-      let dproject = Project({ title: 'Default' });
-      projects.push(dproject);
-      console.log(`Created default project to array`);
+    if (utility.localStorageHasItems('project')) {
+      utility.restoreFromLocalStorage();
+    } else {
+      console.log('adding default obj');
+      addProject(Project({ title: 'Default' }))
     }
   }
-  function getAllProjects() {
-    return projects.forEach(project => console.log(project.getTitle()));
+  function displayAllProjects() {
+    const projectList = [];
+    projects.forEach(project => {
+      projectList.push(project.getTitle());
+    })
+    return projectList;
   }
-  function addProject() {
+  function addProject(projObj) {
+    console.log('adding...', projObj)
+    let status = utility.addToLocalStorage(projObj);
+    if (status != false) {
+      projects.push(projObj);
+    }
+
 
   }
+  const utility = (function () {
+    let projectName = 'project';
+
+    function addToLocalStorage(obj) {
+      if (!localStorage.getItem(projectName)) {
+        localStorage.setItem(projectName, JSON.stringify(obj));
+        console.log('updated localstorage');
+        return;
+      }
+      let projectList = [];
+      const lastStored = JSON.parse(localStorage.getItem('project'));
+      const lastStoredLength = lastStored.length - 1;
+      if (lastStored[lastStoredLength] && obj.title == lastStored[lastStoredLength].title) {
+        console.log('Cannot have duplicate names');
+        return false;
+      }
+      projectList = projectList.concat(lastStored, obj);
+      localStorage.setItem('project', JSON.stringify(projectList));
+      console.log('updated localstorage');
 
 
+    }
+    function restoreFromLocalStorage() {
+      if (localStorageHasItems('project')) {
+        let convertFromLS = JSON.parse(localStorage.getItem('project'));
+        console.log('convertFromLS', convertFromLS)
+        if (convertFromLS['title']) {
+          console.log('the if')
+          projects.push(Project(convertFromLS));
+        } else {
+          console.log('else')
+          convertFromLS.forEach(obj => {
+            projects.push(Project(obj));
+          });
+        }
 
-  return { getAllProjects };
+      }
+    }
+    function localStorageHasItems(key) {
+      return localStorage.getItem(key);
+    }
+    return { addToLocalStorage, restoreFromLocalStorage, localStorageHasItems };
+  })();
+  return { displayAllProjects, projects, initialize, addProject, utility };
 })();
 
-console.log(todoController.getAllProjects());
-console.log(localStorage.getItem('projects'));
+(() => {
+  domController
+  todoController.initialize();
+  todoController.addProject(Project({ title: 'test4' }));
+  console.log('All projects', todoController.projects);
+  console.log('Project: ', todoController.displayAllProjects())
+})();
